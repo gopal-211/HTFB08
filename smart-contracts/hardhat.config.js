b6b18245bcf8e859a001423fc3031a635eba5baf
gopal-211 <learncode973@gmail.com>
2024-02-20 02:15:31 +0530

    hardhat configerd

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@nomiclabs/hardhat-etherscan')
 /** @type import('hardhat/config').HardhatUserConfig */
+const key = '8d5dc68f333ed9ca6b8482e78d21a69a1f50d861b839dbbddee0d3b5e49c8897'
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.9",
+  networks:{
+    mumbai:{
+      url:"https://polygon-mumbai.g.alchemy.com/v2/O4DCybOmylKsCBfBLR5A54dggdgBHj0O",
+      accounts: [key]
+    }
+  }
 };
+
+
+// polygon scan api key : BI2PR6NZ8TJ2EQT5XRH2B7BKZ4RWCSGSZQ
