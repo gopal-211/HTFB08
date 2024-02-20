398ac4a9c8a77c8f81b76a08c7f76826e9c626c9
gopal-211 <learncode973@gmail.com>
2024-02-20 08:53:07 +0530

    added requierd paackages and configerd

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import  {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './index.css';
+import { configureChains, mainnet, WagmiConfig, createClient } from "wagmi";// to get pre build react hooks
 import App from './App';
+import { publicProvider } from "wagmi/providers/public";
+import { polygonMumbai } from '@wagmi/chains';
 import reportWebVitals from './reportWebVitals';
 
+
+
+const { provider, webSocketProvider } = configureChains(
+  [mainnet, polygonMumbai],
+  [publicProvider()]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
+
 ReactDOM.render(
+  <BrowserRouter> 
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <WagmiConfig client={client}>
+   <App/>
+    </WagmiConfig>
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
